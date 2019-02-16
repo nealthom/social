008e6a75f54c5c87970fb328b3056f94980e46fb
thomas <nealthom@gmail.com>
2019-02-15 21:40:58 -0600

    added profile route

---
+++ models/Profile.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+//Create Schema
+const ProfileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+  },
+  handle: {
+    type: String,
+    required: true,
+    max: 40
+  },
+  location: {
+    type: String
+  },
+  topics: {
+    type: [String],
+    required: true
+  },
+  bio: {
+    type: String
+  },
+  social: {
+    youtube: {
+      type: String
+    },
+    twitter: {
+      type: String
+    },
+    facebook: {
+      type: String
+    },
+    linkedin: {
+      type: String
+    },
+    instagram: {
+      type: String
+    }
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = Profile = mongoose.model('profile', ProfileSchema);
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load Profile Model
+const Profile = require('../../models/Profile');
+// Load User Model
+const User = require('../../models/User');
 
 // @route    Get api/profile/test
 // @desc     Tests profile route
@@ -7,5 +14,26 @@ const router = express.Router();
 router.get('/test', (req, res) => {
   res.json({ msg: 'Profile works' });
 });
+
+// @route    Get api/profile
+// @desc     Get current user profile
+// @access   Private
+router.get(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    const errors = {};
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      if (!profile) {
+        errors.noprofile = 'There is no profile for this user';
+        return res.status(404).json(errors);
+      }
+      res.json(profile);
+    } catch (error) {
+      res.status(404).json(error);
+    }
+  }
+);
 
 module.exports = router;
