bd6d0534e2b0fd2ea1ea03ab164d597b62a7c95b
thomas <nealthom@gmail.com>
2019-03-01 17:40:54 -0600

    worked on comments route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -84,28 +84,130 @@ router.delete(
 // @route    Post api/posts/like/:id
 // @desc     Like post
 // @access   Private
-// Work  here -- allows for multiple likes from one person
 router.post(
   '/like/:id',
   passport.authenticate('jwt', { session: false }),
   async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      const index = post.likes.findIndex(like => like.user === req.user.id);
+
+      const index = post.likes.findIndex(like => {
+        return like.user == req.user.id;
+      });
+
       if (index === -1) {
-        post.likes.push({ user: req.user.id });
+        // Add user id to likes array
+        post.likes.push({
+          user: req.user.id
+        });
+        await post.save();
+        res.json(post);
       } else {
         return res
           .status(400)
           .json({ alreadyliked: 'User already liked this post' });
       }
-      // Add user id to likes array
-      post.likes.push({ user: req.user.id });
-      await post.save();
-      res.json(post);
     } catch (error) {
       res.status(404).json({ postnotfound: 'No post found' });
     }
+  }
+);
+
+// @route    Post api/posts/unlike/:id
+// @desc     Unlike post
+// @access   Private
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    try {
+      const post = await Post.findById(req.params.id);
+
+      const index = post.likes.findIndex(like => {
+        return like.user == req.user.id;
+      });
+
+      if (index === -1) {
+        return res
+          .status(400)
+          .json({ notliked: 'User has not liked this post' });
+      } else {
+        // Remove like from array
+        post.likes.splice(index, 1);
+        await post.save();
+        res.json(post);
+      }
+    } catch (error) {
+      res.status(404).json({ postnotfound: 'No post found' });
+    }
+  }
+);
+
+// @route    Post api/posts/comment/:id
+// @desc     Add comment to post
+// @access   Private
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    // Get Post
+    const post = await Post.findById(req.params.id);
+    if (!post) {
+      return res.status(400).json({ postNotFound: 'Post  not found' });
+    }
+
+    const newComment = {
+      text: req.body.text,
+      name: req.user.name,
+      avatar: req.user.avatar,
+      user: req.user.id
+    };
+
+    // Add to comments array
+    post.comments.push(newComment);
+    await post.save();
+    res.json(post);
+  }
+);
+// @route    Delete api/posts/comment/:id/:comment_id
+// @desc    Remove comment from post
+// @access   Private
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    // Get the post
+    const post = await Post.findById(req.user.id);
+    if (!post) {
+      return res.status(400).json({ postNotFound: 'Post  not found' });
+    }
+    // Find the comment to delete
+    const commentIndex = post.comments.findIndex(
+      comment => comment._id == req.params.comment_id
+    );
+    // return if the comment isn't found
+    if (commentIndex === -1) {
+      return res.status(400).json({ commentNotfound: 'Comment not found' });
+    }
+    // Check if the current user is the same as the user of the comment
+    if (post.user != req.user.id) {
+      return res
+        .status(401)
+        .json({ notauthorized: "You can't delete this comment" });
+    }
+
+    // Remove like from array
+    post.comments.splice(commentIndex, 1);
+    await post.save();
+    res.json(post);
   }
 );
 
