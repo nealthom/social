c51a42c377bb34f093f5abb92d53e3c5c3683a66
thomas <nealthom@gmail.com>
2019-03-11 18:00:59 -0500

    worked on navbar and css

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import Navbar from './components/layout/Navbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,8 +1,30 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import Styles from './Navbar.module.css';
 export class Navbar extends Component {
   render() {
-    return <div />;
+    return (
+      <header className={Styles.navbar_header}>
+        {console.log(Styles)}
+        <div>
+          <a href="index.html" className={Styles.main_header__brand}>
+            Social App
+          </a>
+        </div>
+        <nav className={Styles.main_nav}>
+          <ul className={Styles.main_nav__items}>
+            <li className={Styles.main_nav__item}>
+              <a href="profiles.html">Explore Users</a>
+            </li>
+            <li className={Styles.main_nav__item}>
+              <a href="register.html">Sign Up</a>
+            </li>
+            <li className={Styles.main_nav__item}>
+              <a href="login.html">Login</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    );
   }
 }
 
