8127c44560f00d5aa49fd4a85387b3bf66ac425b
thomas <nealthom@gmail.com>
2019-03-15 22:37:35 -0500

    navbar and landing page styling

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+import Styles from './Landing.module.css';
+export class Landing extends Component {
+  render() {
+    return <div className={Styles.overview} />;
+  }
+}
+
+export default Landing;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -3,27 +3,44 @@ import Styles from './Navbar.module.css';
 export class Navbar extends Component {
   render() {
     return (
-      <header className={Styles.navbar_header}>
-        {console.log(Styles)}
-        <div>
-          <a href="index.html" className={Styles.main_header__brand}>
-            Social App
-          </a>
-        </div>
-        <nav className={Styles.main_nav}>
-          <ul className={Styles.main_nav__items}>
-            <li className={Styles.main_nav__item}>
-              <a href="profiles.html">Explore Users</a>
+      <div>
+        <div className={Styles.backdrop} />
+        <nav className={Styles.mobile_nav}>
+          <ul className={Styles.mobile_nav__items}>
+            <li className={Styles.mobile_nav__item}>
+              <a href="packages/index.html">Packages</a>
             </li>
-            <li className={Styles.main_nav__item}>
-              <a href="register.html">Sign Up</a>
-            </li>
-            <li className={Styles.main_nav__item}>
-              <a href="login.html">Login</a>
+            <li className={Styles.mobile_nav__item}>
+              <a href="customers/index.html">Customers</a>
             </li>
           </ul>
         </nav>
-      </header>
+        <header className={Styles.navbar_header}>
+          <div>
+            <button className={Styles.toggle_button}>
+              <span className={Styles.toggle_button__bar} />
+              <span className={Styles.toggle_button__bar} />
+              <span className={Styles.toggle_button__bar} />
+            </button>
+            <a href="index.html" className={Styles.main_header__brand}>
+              N
+            </a>
+          </div>
+          <nav className={Styles.main_nav}>
+            <ul className={Styles.main_nav__items}>
+              <li className={Styles.main_nav__item}>
+                <a href="profiles.html">Explore Users</a>
+              </li>
+              <li className={Styles.main_nav__item}>
+                <a href="register.html">Sign Up</a>
+              </li>
+              <li className={Styles.main_nav__item}>
+                <a href="login.html">Login</a>
+              </li>
+            </ul>
+          </nav>
+        </header>
+      </div>
     );
   }
 }
